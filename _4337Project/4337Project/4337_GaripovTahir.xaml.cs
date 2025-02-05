aa507f1f600a41b64894c597dc89488ae58ce1f8
gtrmalay <garipov.tat@gmail.com>
2025-02-06 02:58:53 +0300

    Исправления

---
+++ _4337Project/4337Project/4337_GaripovTahir.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4337Project
+{
+    /// <summary>
+    /// Логика взаимодействия для _4337_GaripovTahir.xaml
+    /// </summary>
+    public partial class _4337_GaripovTahir : Window
+    {
+        public _4337_GaripovTahir()
+        {
+            InitializeComponent();
+        }
+
+        private void _4337_GaripovTahir1_Click(object sender, RoutedEventArgs e)
+        {
+            _1text.Content = "_1text.Content = GaripovTahir";
+            _2text.Content = "_1text.Content = GaripovTahir";
+        }
+    }
+}
