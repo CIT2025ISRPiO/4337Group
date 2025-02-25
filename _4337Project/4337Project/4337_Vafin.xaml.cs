c4bfbb8dd22a7bcbc3cbe51ba68d3d0e2baa899c
Vooltex <Vooltex.fortnite@mail.ru>
2025-02-25 12:22:20 +0300

    Импорт и экспорт

--- _4337Project/4337Project/4337_Vafin.xaml.cs
+++ _4337Project/4337Project/4337_Vafin.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,43 @@ namespace _4337Project
         public _4337_Vafin()
         {
             InitializeComponent();
+        }
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel Files (*.xlsx)|*.xlsx"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string connectionString = "Server=DESKTOP-3161DTA;Database=LabaISRPO;User Id=your_username;Integrated Security=True;";
+                string tableName = "Orders";
+
+                Vafin.ImportData(filePath, connectionString, tableName);
+
+                MessageBox.Show("Данные успешно импортированы!");
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Excel Files (*.xlsx)|*.xlsx"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string outputFilePath = saveFileDialog.FileName;
+                string connectionString = "Server=DESKTOP-3161DTA;Database=LabaISRPO;User Id=your_username;Integrated Security=True;";
+                string tableName = "Orders";
+
+                Vafin.ExportData(connectionString, tableName, outputFilePath);
+
+                MessageBox.Show("Данные успешно экспортированы!");
+            }
         }
     }
 }
